1820c17aa4905588ad19217b0444d31c0f6f97b5
Shoaib-hacker <79598026+shoaibhacker0786921@users.noreply.github.com>
2021-03-12 14:05:57 +0530

    Add files via upload

---
+++ shoaib-hacker.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#Taking Input & output from user
+echo -e "\e[1;31m 
+===============CODDED BY SHOAIB HACKER==============
+
+===============[ RAMPUR UTTAR PRADESH ]=============
+
+                 CONTACT = 8791334748
+
+===================[ RANDOM TOOL ]==================\e[0m"
+echo -e "\e[1;32m 
+
+CREATED BY = SHOAIB HACKER
+
+
+Tool Name: *[Red Hawk]* number*1*
+______________________________________
+*About Tool:*
+
+All in one tool for Information Gathering and Vulnerabilit>
+
+
+_______________________________________
+*Available For:*
+
+*TERMUX*
+_______________________________________
+*Instalation & Usage:*
+
+$ apt update && apt upgrade
+
+$ apt install git
+
+$ apt install php
+
+$ git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+$ RED_HAWK
+
+$ chmod +x *
+
+$ cd RED_HAWK
+
+$ php rhawk.php
+________________________________________
+◕‿◕ ◕‿◕ ◕‿◕ ◕‿◕ ◕‿◕ ◕‿◕ ◕‿◕ ◕‿◕ ◕‿◕ \e[0m"
